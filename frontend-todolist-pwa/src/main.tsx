02e25c9e56f11baa9a1d4083cf00dd52d33dee92
His000ka <pitroin@LGPierre.localdomain>
2026-03-20 15:52:33 +0100

    fix load resize when starting application

--- frontend-todolist-pwa/src/main.tsx
+++ frontend-todolist-pwa/src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+function fixViewport() {
+  document.documentElement.style.height = window.innerHeight + "px";
+  document.body.style.height = window.innerHeight + "px";
+}
+
+// au load
+window.addEventListener("load", fixViewport);
+window.addEventListener("resize", fixViewport);
+window.addEventListener("orientationchange", fixViewport);
+
+fixViewport();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
